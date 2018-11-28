096cf1cf94d9c99c433c7137ddf51b3741a53c1d
Yura Kuzkin <yurkelove28@gmail.com>
2018-11-28 09:58:16 +0200

    add ui meterial

--- source/components/Registration.tsx
+++ source/components/Registration.tsx
@@ -1,15 +1,45 @@
 import * as React from 'react';
+import {AppBar} from '../../node_modules/@material-ui/core/index';
+import {Tabs} from '../../node_modules/@material-ui/core/index';
+import {Tab} from '../../node_modules/@material-ui/core/index';
+import {Typography} from '../../node_modules/@material-ui/core/index';
+
 
 
 class Registration extends React.Component{
+    state = {
+        value: 'one',
+    };
+
     render (){
+        const { value } = this.state;
         return(
             <div>
-                <h1>Registration Page</h1>
+            <AppBar position="static">
+                <Tabs value={value} onChange={this.handleChange}>
+                    <Tab value="one" label="Войти"/>
+                    <Tab value="two" label="Зарегистрироваться" />
+                </Tabs>
+            </AppBar>
+                {/*{value === 'one' && <TabContainer>*/}
+                  {/*<input type='text' placeholder='введите ваше имя'/>*/}
+                  {/*<input type='text' placeholder='електронная почта'/>*/}
+                {/*</TabContainer>}*/}
+                {/*{value === 'two' && <TabContainer>*/}
+                  {/*<input type='text' placeholder='введите ваше имя'/>*/}
+                  {/*<input type='text' placeholder='електронная почта'/>*/}
+                  {/*<input type='text' placeholder='електронная почта'/>*/}
+                {/*</TabContainer>}*/}
             </div>
+
         );
     }
+
+    handleChange = (event : any, value: any) => {
+        this.setState({ value });
+    };
 }
+
 
 
 export default Registration
