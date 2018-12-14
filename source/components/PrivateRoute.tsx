9f96fa9a5d6b5066a7c38ad55f600b3ae730b84b
Yura Kuzkin <yurkelove28@gmail.com>
2018-12-14 12:58:34 +0200

    merge with -auth2

--- source/components/PrivateRoute.tsx
+++ source/components/PrivateRoute.tsx
@@ -9,11 +9,7 @@ function PrivateRoute({ component: Component,is_authorized, ...rest }:any) {
         {...rest}
         render={ props => {
           return is_authorized ? (<Component {...props} />)
-<<<<<<< HEAD
-          : ( <Redirect to={{pathname: "/registration"}}/>)
-=======
-          : ( <Redirect to="/registration"/>)
->>>>>>> 2449-auth_success
+            : ( <Redirect to="/registration"/>)
         }}
     />
   );
